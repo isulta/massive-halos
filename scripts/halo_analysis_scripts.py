cc5a67a1eef670ba43e643ea13c892d20bc2b50e
Imran Sultan <isultan030@gmail.com>
2021-07-18 18:16:30 -0500

    halo_analysis_scripts: add cosmology code

--- scripts/halo_analysis_scripts.py
+++ scripts/halo_analysis_scripts.py
@@ -13,8 +13,7 @@ def dist(r1, r0):
     return res
 
 def halo_center(coords, masses, shrinkpercent=2.5, minparticles=1000, initialradiusfactor=1, verbose=False):
-    '''See Power et al. 2003.
-    default: shrinkpercent=2.5, minparticles=1000, initialradiusfactor=1
+    '''See Power et al. 2003 (their parameter values: shrinkpercent=2.5, minparticles=1000, initialradiusfactor=1)
     '''
     com = center_of_mass(coords, masses)
 
@@ -55,3 +54,37 @@ def halo_center_wrapper(pdata, shrinkpercent=50, minparticles=1000, initialradiu
     coords = pdata['Coordinates']
     masses = pdata['Masses']
     return halo_center(coords, masses, shrinkpercent, minparticles, initialradiusfactor)
+
+
+### COSMOLOGY CODE ###
+def Ez(OmegaM0, OmegaL0, z):
+    return ( OmegaM0*(1+z)**3 + OmegaL0 )**0.5
+
+def OmegaM(OmegaM0, OmegaL0, z):
+    return OmegaM0 * (1+z)**3 / Ez(OmegaM0, OmegaL0, z)**2
+
+def xz(OmegaM0, OmegaL0, z):
+    return OmegaM(OmegaM0, OmegaL0, z) - 1
+
+def deltavir(OmegaM0, OmegaL0, z):
+    '''Virial overdensity fitting function from Bryan & Norman (1998)'''
+    x = xz(OmegaM0, OmegaL0, z)
+    return 18*np.pi**2 + 82*x - 39*x**2
+
+def rhocritz(OmegaM0, OmegaL0, z):
+    '''Returns the critical density at z in units h^2 Msun/Mpc^3.'''
+    rhocrit0 = 2.77536627e11 #h^2 Msun/Mpc^3
+    return rhocrit0 * Ez(OmegaM0, OmegaL0, z)**2
+
+def Rvir(Mvir, OmegaM0, OmegaL0, z):
+    '''Returns in units of Mpc/h assuming [Mvir]=Msun/h.'''
+    return ( 3/4 * Mvir / (deltavir(OmegaM0, OmegaL0, z) * rhocritz(OmegaM0, OmegaL0, z) * np.pi) )**(1/3)
+
+'''
+def Rvir(coords, masses, com):
+    # TODO
+    r = dist(coords, com)
+    # asort = np.argsort(r)
+    return np.sum(masses[r <= (358.68*0.697)]) #/ (4/3 * np.pi * 358.68**3) * 1e19
+'''
+
