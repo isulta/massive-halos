291b2c0b880d3f18dc937a439898a687e96d831b
Imran Sultan <isultan030@gmail.com>
2021-07-19 04:18:22 -0500

    Add CCA init script

---
+++ scripts/CCA_init.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+cd /home/jovyan/home
+conda install vim htop numpy matplotlib numba git astropy pyyaml
+
+mkdir tools projects
+
+cd tools
+git clone https://github.com/isulta/itk
+git clone https://github.com/agurvich/abg_python
+
+cd ../projects
+git clone https://github.com/isulta/massive-halos
+
+export PYTHONPATH="${PYTHONPATH}:/home/jovyan/home/tools"
+
+cd /home/jovyan/home/projects/massive-halos
+git config --global user.email "isultan030@gmail.com"
+git config --global user.name "Imran Sultan"
+git config --global alias.st status
+git config --global alias.ci commit
