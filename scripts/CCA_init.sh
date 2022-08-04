a15bbad86e0806ff3022474462619971e2c0b69d
Imran Sultan <isultan030@gmail.com>
2022-08-03 19:53:27 -0500

    CCA_init: add screen

--- scripts/CCA_init.sh
+++ scripts/CCA_init.sh
@@ -2,10 +2,11 @@
 set -e
 
 cd /home/jovyan/home
-conda install -y vim htop numpy matplotlib=3.4.2 numba git astropy pyyaml tqdm hdf5
+conda install -y vim htop numpy matplotlib numba git astropy pyyaml tqdm hdf5
 conda install -y -c conda-forge ffmpeg
 conda install -y -c conda-forge silx
 conda install -y -c anaconda joblib
+conda install -y -c conda-forge screen
 
 mkdir tools projects
 
